c05cb551f31ffb5caa6e57d59ef5d103228c67b7
abhijeetw035 <abhijeetw035@gmail.com>
2024-12-16 14:57:02 +0530

    chat list

---
+++ frontend/app/api/users/[userId]/route.ts
@@ -0,0 +1,33 @@
+import Chat from "@models/Chat";
+import User from "@models/User";
+import { connectToDB } from "@mongodb";
+import { NextResponse } from "@node_modules/next/server";
+
+export const GET = async (
+  req: Request,
+  context: { params: { userId: string } }
+) => {
+  try {
+    await connectToDB();
+
+    const { params } = context; // Await params to access userId correctly
+    const { userId } = await params;
+
+    const allChats = await Chat.find({ members: userId })
+      .sort({
+        lastMessageAt: -1,
+      })
+      .populate({
+        path: "members",
+        model: User,
+      })
+      .exec(); 
+      
+      return NextResponse.json(allChats, {status : 200});
+      
+    } catch (error) {
+      console.log(error)
+      return NextResponse.json("Failed to get all chats of current user!", {status : 500});
+    
+  }
+};
--- frontend/components/ChatList.tsx
+++ frontend/components/ChatList.tsx
@@ -1,16 +1,65 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loader from "./Loader";
+import { useSession } from "@node_modules/next-auth/react";
+import ChatBox from "./ChatBox";
 
 const ChatList = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { data: sessions } = useSession();
+  const currentUser = sessions?.user;
+  const [chats, setChats] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getAllChat = async () => {
+    try {
+      let res;
+      if(search !== ''){
+        res = await fetch(`/api/users/${currentUser?.id}/searchChat/${search}`);
+      }
+      else {
+        res = await fetch(`/api/users/${currentUser?.id}`);
+      }
+      const data = await res.json();
+      setChats(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (currentUser) {
+      getAllChat();
+    }
+    console.log(search);
+  }, [currentUser, search]);
+
+  console.log(chats);
 
   return loading ? (
     <Loader />
   ) : (
     <div className="chat-list">
-      <input placeholder="Search chat..." className="input-search" />
+      <input
+        placeholder="Search chat..."
+        className="input-search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <div className="chats">
+        {chats?.map((chat, index) => (
+          <ChatBox
+            key={index}
+            index={index}
+            chat={chat}
+            currentUser={currentUser}
+            // currentChatId={currentChatId}
+          />
+        ))}
+      </div>
     </div>
   );
 };
